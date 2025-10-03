af1fa9c6dde7e44f0e156ff8e942e3e88550c802
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-10-03 22:56:04 +0530

    Quantum AI: Search Engine: initial setup

---
+++ src/main/java/com/ranjan/quantumaisearchengine/QuantumaisearchengineApplication.java
@@ -0,0 +1,13 @@
+package com.ranjan.quantumaisearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumaisearchengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumaisearchengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranjan/quantumaisearchengine/QuantumaisearchengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjan.quantumaisearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumaisearchengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
